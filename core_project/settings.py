5ee2cf75a0fbe7c3d86a20622f332ddc1b3f0065
Ahmed@Atef <clay674302@gmail.com>
2025-12-13 07:27:16 +0200

    refactor: Remove unused authentication settings and allauth URLs
    - Deleted allauth middleware and authentication backends from settings.py.
    - Removed accounts URL pattern from urls.py to streamline project configuration.

--- core_project/settings.py
+++ core_project/settings.py
@@ -52,18 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # allauth account middleware:
-    "allauth.account.middleware.AccountMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by email
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
 
 
 ROOT_URLCONF = 'core_project.urls'
@@ -138,17 +128,3 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
-# Provider specific settings
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
-    }
-}
